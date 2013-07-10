e91e22f48347fe3267854391293e69e5d1ba05f8
Gabriel Brenner <gabejbrenner@gmail.com>
2013-07-10 05:05:10 +0200

    initialise Rails server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AndroidCustomNotification::Application.config.secret_key_base = 'a46ab349a1db46692bf010d9f137b4b3af8719da9cffff67ed14967473d52d06ab38c6138340e99782f6c6a8d5e2c2c88aedc5d2a50f749798ee603507546165'
